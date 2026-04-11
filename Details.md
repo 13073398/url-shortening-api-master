44e40764ea1c19b52c7123db02f379cf9624a4f5
13073398 <mosamirsultan1985@gmail.com>
2026-04-11 11:19:35 +0200

    version one

---
+++ Details.md
@@ -0,0 +1,62 @@
+# Frontend Mentor - Shortly URL shortening API Challenge solution
+
+This is a solution to the [URL shortening API Challenge](https://www.frontendmentor.io/challenges/url-shortening-api-landing-page-2ce3ob-G) on Frontend Mentor.
+
+## Table of Contents
+
+- [Overview](#overview)
+- [Features](#features)
+- [Technologies Used](#technologies-used)
+- [Installation](#installation)
+- [Usage](#usage)
+- [Contributing](#contributing)
+- [License](#license)
+- [Acknowledgments](#acknowledgments)
+
+## Overview
+
+This project is a solution to the URL shortening API Challenge on Frontend Mentor. It allows users to shorten any valid URL and view a list of their shortened links. The project is built using HTML, CSS, and JavaScript.
+
+## Features
+
+- Shorten any valid URL
+- View a list of shortened links
+- Copy the shortened link to the clipboard in a single click
+- Responsive design for different screen sizes
+
+## Technologies Used
+
+- HTML
+- CSS
+- JavaScript
+- Bootstrap
+-API
+-axsios
+
+## Installation
+
+1. Clone the repository: `git clone https://github.com/your-username/project-name.git`
+2. Navigate to the project directory: `cd project-name`
+3. Open the project in your preferred code editor.
+
+## Usage
+
+1. Open the `index.html` file in your web browser.
+2. Enter a URL in the input field and click the "Shorten It!" button.
+3. The shortened link will be displayed in the "Shorten URL" section.
+4. Click the "Copy" button to copy the shortened link to your clipboard.
+5. Refresh the page to see the list of shortened links.
+
+## Contributing
+
+Contributions are welcome! If you find any issues or have suggestions for improvements, please open an issue or submit a pull request.
+
+## License
+
+This project is licensed under the [MIT License](LICENSE).
+
+## Acknowledgments
+
+- Frontend Mentor for providing the challenge.
+- Bootstrap for the responsive design.
+- Stack Overflow for help with troubleshooting.
---
+++ logic.js
@@ -0,0 +1,113 @@
+// get the shorten button and shorten input
+const shorten_button=document.getElementById("shorten_btn");
+const shorten_input=document.getElementById("shorten_input");
+// get the area where the shortened links will be posted
+let post_area=document.getElementById("shorten_url") ;
+
+   mohamed=JSON.parse(window.localStorage.getItem("btn_copy_id"))
+// initialize an empty array to store local storage
+let local_store_arry=[];
+let btn_copied_id=[]
+
+
+// create a params object with an empty url
+let params={
+    "url":""
+};
+
+// check if there is already local storage
+if(window.localStorage.getItem("shorten_url")){
+    // parse the local storage and store it in the local_store_arry
+    local_store_arry=JSON.parse(window.localStorage.getItem("shorten_url"));
+    // loop through the local_store_arry and add each item to the post_area
+    for(let i=0;i<local_store_arry.length;i++){
+        post_area.innerHTML+=`
+        <div class="container-xl" id='${i+1}'>
+            <div class="left"><h6>https://www.Frontendmentor.io</h6></div>
+            <div class="right">
+              <div class="text" ><h6>${local_store_arry[i]}</h6></div>
+              <div class="tool" id='${i+1}'><button ' class="btn btn-secondary ms-3 " title="Copy" id='${i+1}' >Copy</button></div>
+            </div>
+        </div>
+    `
+    }
+   
+}
+
+// add an event listener to the shorten button
+shorten_button.addEventListener("click",()=>{
+    // set the url of the params object to the value of the shorten input
+    params.url=shorten_input.value;
+    const proxy = "https://corsproxy.io/?";
+    // make a POST request to the proxy
+    axios.post(proxy+"https://cleanuri.com/api/v1/shorten",params).then((pass)=>{
+        // add the shortened link to the local_store_arry
+        local_store_arry.push(pass.data.result_url);
+        // update the local storage
+        window.localStorage.setItem("shorten_url",JSON.stringify(local_store_arry))
+        // add the shortened link to the post_area
+        post_area.innerHTML+=`
+        <div class="container-xl">
+            <div class="left"><h6>https://www.Frontendmentor.io</h6></div>
+            <div class="right">
+              <div class="text" ><h6>${pass.data.result_url}</h6></div>
+              <div class="tool"><button class="btn btn-secondary ms-3 " title="Copy">Copy</button></div>
+            </div>
+        </div>
+    `
+    location.reload();
+    shorten_input.value="";
+    
+    
+}).catch((error)=>{
+    // if there is an error, add an error message to the post_area
+    post_area.innerHTML+=`
+        <div class="container-xl" style="justify-content:center!important;">
+            
+            <div class="right">
+              <div class="text" style="color:red" ><h6>wrong Error</h6></div>
+    
+            </div>
+        </div>
+    `;
+});
+});
+
+
+// get all the copy buttons
+let copy_buttons = document.querySelectorAll(" [title='Copy']");
+// loop through the copy buttons and add an event listener to each one
+copy_buttons.forEach((ele) => {
+    ele.addEventListener("click", (e) => {
+        // get the text of the shortened link
+        let text = e.currentTarget.parentElement.previousElementSibling.textContent;
+        // copy the text to the clipboard
+        navigator.clipboard.writeText(text);
+        // add a class of 'copied' to the button
+        e.currentTarget.classList.add("copied");
+        // update the text of the button to 'Copied!'
+        e.currentTarget.innerHTML="Copied!";
+         e.currentTarget.style.disabled=false;
+        // store the id of the copied button in the btn_copied_id array
+        e.currentTarget.id=e.currentTarget.parentElement.id;
+        btn_copied_id=JSON.parse(window.localStorage.getItem("btn_copy_id"))
+        btn_copied_id.push(e.currentTarget.parentElement.id)    
+        window.localStorage.setItem("btn_copy_id", JSON.stringify(btn_copied_id));
+    }); 
+});
+
+// loop through all the copy buttons and check if their id is in the btn_copied_id array
+window.addEventListener("DOMContentLoaded", () => {
+  const btn_copy_id = JSON.parse(window.localStorage.getItem("btn_copy_id"));
+   document.querySelectorAll(".right .tool .btn").forEach((ele)=>{
+        for(let i=0;i<btn_copy_id.length;i++){
+           
+            if(ele.id===btn_copy_id[i]){
+                ele.classList.add("copied");
+                ele.innerHTML="copied !"
+                
+            }   
+        }
+   })
+});
+
